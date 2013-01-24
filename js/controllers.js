cb1e6e41f2a3ae7a4b99c0a2746294ff69d93d32
Alexander Kohlhofer <alexander@kohlhofer.com>
2013-01-24 17:28:51 -0500

    now allowing to toggle between html source and rendered html

--- js/controllers.js
+++ js/controllers.js
@@ -5,13 +5,22 @@ function editorController($scope) {
 
   $scope.title ="Hello World";
 
-  $scope.markDownDocument = "test";
+  $scope.markDownDocument = "";
+
+  $scope.showSource = false;
+
+  $scope.toggleSource = function() {
+    if ($scope.showSource == false) {
+      $scope.showSource = true;
+    } else {
+      $scope.showSource = false;
+    }
+  }  
 
   $scope.convertMarkDown = function(source) {
-
     return converter.makeHtml(source);
   }
-  
+
 }
 
 editorController.$inject = ['$scope'];
